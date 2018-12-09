5272ce2e0330dfde759a232ceb6dcc2d2b9e7407
patricliu <pfliu2@illinois.edu>
2018-12-09 15:58:19 -0600

    ipynb updates

--- getdata.py
+++ getdata.py
@@ -6,27 +6,7 @@ path = os.getcwd() + '/xray_images'
 test_size = 3999
 train_size = 19999 - test_size
 
-'''def get_training(flatten = False, amount = train_size, rescale = True):
-	if not flatten:
-		data_x = np.zeros((amount,64,64))
-		data_y = np.zeros((amount,128,128))
-		for n, index in enumerate(list(range(test_size + 1, amount + test_size + 1))):
-			x_path = path + '/train_images_64x64/train_' + ('00000' + str(index))[-5:] + '.png'
-			y_path = path + '/train_images_128x128/train_' + ('00000' + str(index))[-5:] + '.png'
-			data_x[n] = cv2.imread(x_path, cv2.IMREAD_GRAYSCALE)
-			data_y[n] = cv2.imread(y_path, cv2.IMREAD_GRAYSCALE)
-		return data_x,data_y
-	elif flatten:
-		data_x = np.zeros((amount,64*64))
-		data_y = np.zeros((amount,128*128))
-		for n, index in enumerate(list(range(test_size + 1, amount + test_size + 1))):
-			x_path = path + '/train_images_64x64/train_' + ('00000' + str(index))[-5:] + '.png'
-			y_path = path + '/train_images_128x128/train_' + ('00000' + str(index))[-5:] + '.png'
-			data_x[n] = cv2.imread(x_path, cv2.IMREAD_GRAYSCALE).flatten()
-			data_y[n] = cv2.imread(y_path, cv2.IMREAD_GRAYSCALE).flatten()
-		return data_x,data_y'''
-
-def load(sub_path, rng, array, flatten, rescale, denoise_only = False):
+def load(sub_path, rng, array, flatten, rescale, denoise_only):
 	for n, index in enumerate(rng):
 		full_path = sub_path + ('00000' + str(index))[-5:] + '.png'
 		img = img = cv2.imread(full_path, cv2.IMREAD_GRAYSCALE)
@@ -41,7 +21,9 @@ def load(sub_path, rng, array, flatten, rescale, denoise_only = False):
 		array[n] = img
 	return array
 
-def get_training(flatten = False, amount = train_size, rescale = True, denoise_only = True):
+def get_training(amount, flatten = False, rescale = True, denoise_only = False):
+	if amount == 0:
+		amount = train_size
 	rng = list(range(test_size + 1, test_size + amount + 1))
 	data_x = None
 	data_y = None
@@ -58,19 +40,20 @@ def get_training(flatten = False, amount = train_size, rescale = True, denoise_o
 		else:
 			data_y = np.zeros((amount,128,128,1))
 
-	data_x = load(path + '/train_images_64x64/train_', rng, data_x, flatten, rescale)
+	data_x = load(path + '/train_images_64x64/train_', rng, data_x, flatten, rescale, denoise_only)
 	data_y = load(path + '/train_images_128x128/train_', rng, data_y, flatten, rescale, denoise_only)
 
 	return data_x, data_y
 
 
-def get_test(flatten = False, amount = test_size, rescale = True):
-	rng = list(range(1, amount + 1))
+def get_test(amount, flatten = False, rescale = True, denoise_only = False, sample = 0):
+	if amount == 0:
+		amount = test_size
+	rng = list(range(sample + 1, sample + amount + 1))
+	data_x = None
 	if not flatten:
 		data_x = np.zeros((amount,64,64,1))
-		data_x = load(path + '/test_images_64x64/test_', rng, data_x, False, rescale)
-		return data_x
 	else:
 		data_x = np.zeros((amount,64*64))
-		data_x = load(path + '/test_images_64x64/test_', rng, data_x, True, rescale)
-		return data_x
+	data_x = load(path + '/test_images_64x64/test_', rng, data_x, flatten, rescale, denoise_only)
+	return data_x, rng
