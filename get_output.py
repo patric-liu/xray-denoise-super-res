b605eda23c4a9a95a7f4ba25edfba1e0681ac2d6
patricliu <pfliu2@illinois.edu>
2018-12-13 17:07:36 -0600

    get_output

--- get_output.py
+++ get_output.py
@@ -10,7 +10,7 @@ from keras.models import model_from_json, Model
 from keras.callbacks import TensorBoard
 
 # Load Model
-model_name = 'model1'
+model_name = 'model3'
 
 # model reconstruction from JSON
 net = network.Network(None, None, None, test = 0)
